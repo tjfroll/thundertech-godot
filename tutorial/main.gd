042324d7ef67fea9924c181a18699b92c1f430a0
FRESHFUNK\tjfro <tjfroll@gmail.com>
2025-07-14 23:03:40 -0400

    Complete tutorial

--- tutorial/main.gd
+++ tutorial/main.gd
@@ -9,11 +9,18 @@ func _ready():
 func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
+	$Music.stop()
+	$Death.play()
 	
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message('git rdy')
+	get_tree().call_group("mobs", "queue_free")
+	$Music.play()
 
 func _on_mob_timer_timeout() -> void:
 	var mob = mob_scene.instantiate()
@@ -33,3 +40,4 @@ func _on_start_timer_timeout() -> void:
 
 func _on_score_timer_timeout() -> void:
 	score += 1
+	$HUD.update_score(score)
